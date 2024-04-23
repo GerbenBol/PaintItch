221e9eccb29b1fe0a2705870486eed552db7e9a7
Blaze-Darkwood <darkwoodblaze@gmail.com>
2024-04-23 12:10:45 +0200

    Long forgotten reload push

--- Assets/Scripts/Player/PlayerPainting.cs
+++ Assets/Scripts/Player/PlayerPainting.cs
@@ -13,28 +13,44 @@ public class PlayerPainting : MonoBehaviour
     [SerializeField] private ParticleSystem shootParticle;
     [SerializeField] private Texture texture;
 
+    private float maxAmmo = 8;
+    [SerializeField] private float ammo;
+    private bool reloading;
+
+    private float ammoBar;
+
     private void Start()
     {
-        Colors = new() { Color.red, Color.cyan, Color.blue }; 
+        Colors = new() { Color.red, Color.cyan, Color.blue };
+        ammo = maxAmmo;
     }
 
     private void Update()
     {
         // Shooting
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && ammo > 0 && !reloading)
             Shoot();
         else if (shootParticle.isPlaying)
             shootParticle.Stop();
+
+        // Reloading
+        if (Input.GetKeyDown(KeyCode.R) && !reloading)
+            StartCoroutine(nameof(Reload));
 
         // Color scroll
         if (Input.mouseScrollDelta.y > 0)
             NextColor();
         else if (Input.mouseScrollDelta.y < 0)
             PrevColor();
+
+        // Update visuals for ammo bar
+        ammoBar = maxAmmo / ammo;
     }
 
     private void Shoot()
     {
+        ammo -= Time.deltaTime;
+
         if (shootParticle.isStopped)
             shootParticle.Play();
     }
@@ -53,5 +69,13 @@ public class PlayerPainting : MonoBehaviour
 
         if (ActiveColor < 0)
             ActiveColor = Colors.Count - 1;
+    }
+
+    private IEnumerator Reload()
+    {
+        reloading = true;
+        yield return new WaitForSeconds(2);
+        ammo = maxAmmo;
+        reloading = false;
     }
 }

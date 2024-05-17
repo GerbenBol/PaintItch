9e767f8618bec348e5ddaf2c637a0a1de52867f4
Gerben Bol <gerben.bol249@gmail.com>
2024-05-17 15:06:53 +0200

    shooting system working, not perfect yet

--- Assets/Scripts/Player/PaintingSystem.cs
+++ Assets/Scripts/Player/PaintingSystem.cs
@@ -6,8 +6,10 @@ public class PaintingSystem : MonoBehaviour
 {
     [SerializeField] private GameObject paintPrefab;
     [SerializeField] private Transform spawnPoint;
+    [SerializeField] private Transform mainCam;
+    [SerializeField] private float raycastlength;
 
-    private readonly float maxTimer = .1f;
+    private readonly float maxTimer = .05f;
     private float timer = .0f;
 
     private void Update()
@@ -16,7 +18,9 @@ public class PaintingSystem : MonoBehaviour
             timer += Time.deltaTime;
         else
         {
-            if (Input.GetMouseButton(0))
+            bool raycast = Physics.Raycast(mainCam.position, mainCam.forward, raycastlength);
+
+            if (Input.GetMouseButton(0) && !raycast)
             {
                 Shoot();
                 timer = .0f;
@@ -26,10 +30,20 @@ public class PaintingSystem : MonoBehaviour
 
     private void Shoot()
     {
+        // Create splatter
         GameObject splatterObject = Instantiate(paintPrefab, spawnPoint.position, spawnPoint.rotation);
         PaintSplatter splatter = splatterObject.GetComponent<PaintSplatter>();
-        Vector3 force = new(0, 0, 1000);
-        Vector3 scale = new(1, 1, 1);
+
+        // Determine starting velocity and scale (I use System.Random because it's simply that much better than Unity's Random)
+        System.Random rand = new();
+        int randomForceX = rand.Next(-50, 50);
+        int randomForceY = rand.Next(-50, 50);
+        int randomForceZ = rand.Next(300, 600);
+        float randomScale = (float)rand.Next(15, 40) / 100;
+
+        // Set the starting velocity and scale and send the splatter flying
+        Vector3 force = new(randomForceX, randomForceY, randomForceZ);
+        Vector3 scale = new(randomScale, randomScale, randomScale);
         splatter.Send(Color.red, force, scale);
     }
 }

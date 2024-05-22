f49b8df50d365c5a3c98a8e78693f7a3e27fc3ee
Gerben Bol <gerben.bol249@gmail.com>
2024-05-22 12:13:21 +0200

    start shop

---
+++ Assets/Scripts/UI/Shop/BuyableItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyableItem : MonoBehaviour
+{
+    public string Name;
+    public int Cost;
+    public bool Bought;
+}

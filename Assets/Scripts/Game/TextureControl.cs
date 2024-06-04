3d69af8aa97e708b958649de93fce494f1c9a54c
Gerben Bol <gerben.bol249@gmail.com>
2024-06-04 11:45:16 +0200

    calculator

--- Assets/Scripts/Game/TextureControl.cs
+++ Assets/Scripts/Game/TextureControl.cs
@@ -7,7 +7,10 @@ public class TextureControl : MonoBehaviour
     [SerializeField] private AudioClip dingClip;
 
     public static List<Texture2D> ToUpdate = new();
+    public static List<PaintableObject> ToCalculate = new();
     public static AudioClip CompletedDing;
+
+    private static int index = 0;
 
     private readonly float maxTimer = .1f;
     private float timer = .0f;
@@ -28,6 +31,42 @@ public class TextureControl : MonoBehaviour
             timer = .0f;
         }
         else timer += Time.deltaTime;
+
+        if (index < ToCalculate.Count)
+            StartNextCalc();
+    }
+    
+    private void StartNextCalc()
+    {
+        PaintableObject obj;
+        int checkAmount = 500000;
+        int returner = 0;
+
+        while (index < ToCalculate.Count - 1)
+        {
+            obj = ToCalculate[index];
+
+            if (obj.CompareTag("Level" + GameManagerScript.CurrentLevel))
+            {
+                returner = obj.CalculatePercentage(checkAmount);
+
+                if (returner == -1)
+                {
+                    index--;
+                    break;
+                }
+                else
+                    checkAmount = returner;
+            }
+
+            if (returner == -1)
+                break;
+
+            index++;
+        }
+
+        if (index + 1 < ToCalculate.Count)
+            index++;
     }
 
     private IEnumerator ApplyTextures()

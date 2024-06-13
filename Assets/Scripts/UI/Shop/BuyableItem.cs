ced653684c29d3e3b8d9a44699549a1aecd99783
Gerben Bol <gerben.bol249@gmail.com>
2024-06-13 11:45:58 +0200

    buy done

--- Assets/Scripts/UI/Shop/BuyableItem.cs
+++ Assets/Scripts/UI/Shop/BuyableItem.cs
@@ -1,10 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BuyableItem : MonoBehaviour
 {
     public string Name;
-    public int Cost;
+    public double Cost;
+    public bool MultipleBuyable;
     public bool Bought;
+
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI costText;
+
+    private void Start()
+    {
+        nameText.text = Name;
+        costText.text = Cost.ToString();
+    }
+
+    public void Buy()
+    {
+        if (!MultipleBuyable)
+        {
+            costText.text = "Bought!";
+            Bought = true;
+        }
+    }
 }
